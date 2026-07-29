2901df394c2d789a66d078cf53300c4a9d1636a8
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-29 11:17:28 +0200

    Update EffectTask.java

--- src/main/java/fr/blooditems/managers/EffectTask.java
+++ src/main/java/fr/blooditems/managers/EffectTask.java
@@ -1,1 +1,75 @@
+package fr.blooditems.managers;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class EffectTask {
+
+
+    private final ArmorManager armorManager;
+    private final ItemManager itemManager;
+
+
+    public EffectTask(ArmorManager armorManager, ItemManager itemManager) {
+
+        this.armorManager = armorManager;
+        this.itemManager = itemManager;
+
+    }
+
+
+
+    public void start() {
+
+        Bukkit.getScheduler().runTaskTimer(
+                Bukkit.getPluginManager()
+                        .getPlugin("BloodItems"),
+
+                () -> {
+
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+
+                        armorManager.applyHelmetEffect(
+                                player,
+                                itemManager.getItemId(
+                                        player.getInventory().getHelmet()
+                                )
+                        );
+
+
+                        armorManager.applyChestplateEffect(
+                                player,
+                                itemManager.getItemId(
+                                        player.getInventory().getChestplate()
+                                )
+                        );
+
+
+                        armorManager.applyLeggingsEffect(
+                                player,
+                                itemManager.getItemId(
+                                        player.getInventory().getLeggings()
+                                )
+                        );
+
+
+                        armorManager.applyBootsEffect(
+                                player,
+                                itemManager.getItemId(
+                                        player.getInventory().getBoots()
+                                )
+                        );
+
+                    }
+
+
+                },
+
+                0L,
+
+                20L
+        );
+
+    }
+
+}
